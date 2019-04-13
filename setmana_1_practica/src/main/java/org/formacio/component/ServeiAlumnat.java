df2081ca481fa5be109a789f9d93da11ca172aab
Carlos Alhama <patezno97@gmail.com>
2019-04-13 15:35:54 +0200

    added project

---
+++ setmana_1_practica/src/main/java/org/formacio/component/ServeiAlumnat.java
@@ -0,0 +1,19 @@
+package org.formacio.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ServeiAlumnat {
+
+	
+	/**
+	 * ha de donar d'alta a la base de dades d'alumnes l'alumne indicat amb 
+	 * el corresponent codi.
+	 * Si el nom de l'alumne es null, no l'ha de donar d'alta
+	 * Retorna true si l'alumne s'ha inserit, false si no.
+	 */
+	public boolean matricula (int id, String alumne) {
+		return true;
+	}
+	
+}
