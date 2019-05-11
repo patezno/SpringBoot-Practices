1116a09ba4140db56d42903ff6562d0f6013aaf4
Carlos Alhama <patezno97@gmail.com>
2019-05-11 13:51:39 +0200

    caso test superado

---
+++ 3_api_basica_exercici/src/main/java/org/formacio/api/Localitat.java
@@ -0,0 +1,49 @@
+package org.formacio.api;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Introduiu, sense canviar el codi ja existent, les annotacions de mapping necessaries
+ * per tal que aquesta classe sigui una entitat mapejada a la taula T_LOCALITATS tal com
+ * esta definida a:
+ * src/main/resources/schema.sql
+ */
+@Entity
+@Table(name="t_localitats")
+public class Localitat {
+
+	@Id
+	@Column(name="loc_id")
+	private Long id;
+	
+	@Column(name="loc_nom")
+	private String nom;
+
+	@Column(name="loc_habs")
+	private Integer habitants;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public Integer getHabitants() {
+		return habitants;
+	}
+	public void setHabitants(Integer habitants) {
+		this.habitants = habitants;
+	}
+	
+	
+	
+}
---
+++ 3_api_basica_exercici/src/main/java/org/formacio/api/LocalitatOpBasic.java
@@ -0,0 +1,33 @@
+package org.formacio.api;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * Modifica aquesta classe per tal que sigui un component Spring que realitza les 
+ * operacions de persistencia tal com indiquen les firmes dels metodes
+ */
+
+@Component
+public class LocalitatOpBasic {
+	
+	@PersistenceContext
+	private EntityManager em;
+	
+	public Localitat carrega (long id) {
+		return em.find(Localitat.class, id);
+	}
+	
+	public void alta (String nom, Integer habitants) {
+	}
+	
+	public void elimina (long id) {
+	}
+	
+	public void modifica (Localitat localitat) {
+	}
+
+}
