8fef4c9ddb7dc1cb8e9f1d98e1f2ec971444f9eb
Carlos Alhama <patezno97@gmail.com>
2019-05-18 19:25:16 +0200

    proyecto de verdad importado

--- 10_setmana_2_exercici/src/main/java/org/formacio/setmana2/JpaBasicApplication.java
+++ 10_setmana_2_exercici/src/main/java/org/formacio/setmana2/JpaBasicApplication.java
@@ -1,6 +1,4 @@
-package org.formacion;
-
-import java.io.IOException;
+package org.formacio.setmana2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JpaBasicApplication {
 
-
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args)  {
 		SpringApplication.run(JpaBasicApplication.class, args);
 	}
+
+
 }
--- 6_no_service/src/test/resources/insert.sql
+++
@@ -1,3 +0,0 @@
-delete from T_COMPTES;
-insert into T_COMPTES (COM_CLIENT, COM_QTAT) values ('Aina', 100);
-insert into T_COMPTES (COM_CLIENT, COM_QTAT) values ('Pau', 100);
